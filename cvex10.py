a63e66973d9ff1fde48d9e77353a4e7857670dfb
pumca2025 <pumca2025@gmail.com>
2026-01-21 15:40:11 +0530

    completed

--- cvex10.py
+++ cvex10.py
@@ -3,14 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import time
 from sklearn.datasets import load_digits
-digits = load_digits
+digits = load_digits()
 print(digits.keys())
 print("Label Data Shape",digits.target.shape)
 x=digits.images
-from sklearn.metrices import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.neighbours import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 import seaborn as sns
 X=digits.data
 y=digits.target
@@ -18,11 +18,12 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0
 knn=OneVsRestClassifier(KNeighborsClassifier())
 knn.fit(X_train,y_train)
 predictions=knn.predict(X_test)
-print('KNN Accuracy :%.3f % accuracy_score(y_test,predictions)')
+print('KNN Accuracy : %.3f' % accuracy_score(y_test,predictions))
+cm=confusion_matrix(y_test,predictions)
 plt.figure(figsize=(9,9))
-sns.heatmap(cm,annot=True,fmt='.3f',linewidths=.5,square=True,cmap='Blue_r')
+sns.heatmap(cm,annot=True,fmt='d',linewidths=.5,square=True,cmap='Blues_r')
 plt.ylabel('Actual label')
 plt.xlabel("Predicted Label")
-all_sample_title='Accuracy_Scjore:{0}'.format(accuracy_score(y_test,predictions))
+all_sample_title='Accuracy_Score:{0}'.format(accuracy_score(y_test,predictions))
 plt.title(all_sample_title,size=15)
-
+plt.show()
